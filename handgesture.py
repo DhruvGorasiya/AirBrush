01617975db3e91e60f34b34676e62cb82665e1c7
DhruvGorasiya <Dhruv.Gorasiya@student.csulb.edu>
2024-12-18 20:42:52 -0500

    Update handgesture.py

--- handgesture.py
+++ handgesture.py
@@ -114,7 +114,6 @@
 
 # cap.release()
 
-
 import mediapipe as mp
 import cv2
 import math
@@ -125,7 +124,7 @@ mp_drawing = mp.solutions.drawing_utils
 
 cap = cv2.VideoCapture(0)
 
-pts = []
+pts = [[]]
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -134,6 +133,9 @@ while cap.isOpened():
 
     # Convert the BGR image to RGB
     image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # image = cv2.flip(_image,1)
+
     results = hands.process(image)
 
     # Draw the hand landmarks on the image
@@ -144,6 +146,7 @@ while cap.isOpened():
         # print(len(results.multi_hand_landmarks))
         if len(results.multi_hand_landmarks) < 2:
             for hand_landmarks in results.multi_hand_landmarks:
+
                 fx, fy = int(hand_landmarks.landmark[8].x * frame.shape[1]), int(
                     hand_landmarks.landmark[8].y * frame.shape[0]
                 )
@@ -157,16 +160,24 @@ while cap.isOpened():
 
                 cv2.circle(frame, (fx, fy), 5, (255, 0, 255), cv2.FILLED)
 
-                pts.append((fx, fy))
-
-                if len(pts) > 1:
-                    for i in range(1, len(pts)):
-                        cv2.line(frame, pts[i - 1], pts[i], (255, 0, 0), 5)
+                if distance < 80:
+                    pts[-1].append((fx, fy))
+                    if len(pts[-1]) > 1:
+                        for i in range(1, len(pts[-1])):
+                            cv2.line(frame, pts[-1][i - 1], pts[-1][i], (255, 0, 0), 5)
+                else:
+                    if len(pts[-1]) != 0:
+                        pts.append([])
 
                 # print(hand_landmarks.landmark[1])
                 mp_drawing.draw_landmarks(
                     frame, hand_landmarks, mp_hands.HAND_CONNECTIONS
                 )
+
+    for i in pts:
+        if len(i) > 1:
+            for j in range(1, len(i)):
+                cv2.line(frame, i[j - 1], i[j], (255, 0, 0), 5)
 
     cv2.imshow("Hand Tracking", frame)
     if cv2.waitKey(10) == ord("q"):
